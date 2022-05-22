1c90129d5afa8ff1e43793686421de3a482fdea0
sexyoung <Habuche@gmail.com>
2022-05-22 13:35:07 +0800

    CI(db): setting prisma connection for dev env

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>歡迎來到 Remix!</h1>
+      <h1>今天是個好天氣 Remix!</h1>
       <ul>
         <li>
           <a
