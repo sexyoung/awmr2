b8e9d7c49ecab3872a61e5cb9a5ee562c0b92452
sexyoung <Habuche@gmail.com>
2022-08-04 01:51:46 +0800

    style(d): menu for mobile

--- app/routes/d.tsx
+++ app/routes/d.tsx
@@ -1,5 +1,6 @@
 import { LoaderFunction, json, LinksFunction } from "@remix-run/node";
 import { Outlet, Link, useLoaderData, Form } from "@remix-run/react";
+import { MouseEventHandler } from "react";
 
 import { getUser, requireUserId } from "~/api/user";
 import { Role } from "~/consts/role";
@@ -28,43 +29,55 @@ export default function DashBoard() {
   const handleLogout: React.FormEventHandler<HTMLFormElement> = (e) => {
     !confirm('確定登出?') && e.preventDefault();
   }
+
+  const handleClose: MouseEventHandler = (e) => {
+    const hamburger = document.getElementById('hamburger') as HTMLInputElement;
+    
+    if(hamburger.checked && e.target === document.getElementById('menu') || (e.target as HTMLElement).tagName === 'A') {
+      hamburger.checked = false;
+    }
+  }
   
   return (
     <div className="frame">
       <div className="app">
-        <div className="menu">
+        <input type="checkbox" id="hamburger" />
+        <div id="menu" className="menu xs:pf xs:df xs:jcsb" onClick={handleClose}>
           <div className="system-name">
             小區
             <u>抄表系統</u>
             <small><small><small>v1.5</small></small></small>
           </div>
-          {user &&
-            <div className="user-info">
-              <div className="avatar" style={{backgroundImage: `url(/avatar/${user.avatar})`}}/>
-              <div className="name">{user.name}</div>
-              <Form action="/logout" method="post" onSubmit={handleLogout}>
-                <button type="submit" className="logout">
-                  登出
-                </button>
-              </Form>
-            </div>
-          }
-          <ul>
-            {user?.title !== Role.ENG && <>
-              <li><Link to="/d">首頁總覽</Link></li>
-              <li><Link to="/d/area">小區查詢</Link></li>
-              <li><Link to="/d/project">標案管理</Link></li>
-              <li><Link to="/d/meter/upload">上傳水錶</Link></li>
-              <li><Link to="/d/meter">水錶查詢</Link></li>
-            </>}
+          <label htmlFor="hamburger" id="hamburger-menu" />
+          <div className="info bg-blue-bayoux xs:pf xs:r0 xs:t0 xs:h100vh">
+            {user &&
+              <div className="user-info">
+                <div className="avatar" style={{backgroundImage: `url(/avatar/${user.avatar})`}}/>
+                <div className="name">{user.name}</div>
+                <Form action="/logout" method="post" onSubmit={handleLogout}>
+                  <button type="submit" className="logout">
+                    登出
+                  </button>
+                </Form>
+              </div>
+            }
+            <ul>
+              {user?.title !== Role.ENG && <>
+                <li><Link to="/d">首頁總覽</Link></li>
+                <li><Link to="/d/area">小區查詢</Link></li>
+                <li><Link to="/d/project">標案管理</Link></li>
+                <li><Link to="/d/meter/upload">上傳水錶</Link></li>
+                <li><Link to="/d/meter">水錶查詢</Link></li>
+              </>}
 
-            <li><Link to="/d/record">水錶登錄</Link></li>
-            <li><Link to="/d/record/history">登錄記錄</Link></li>
-            {user?.title !== Role.ENG && <>
-              <li><Link to="/d/record/out">區外要求</Link></li>
-              <li><Link to="/d/user">人事查詢</Link></li>
-            </>}
-          </ul>
+              <li><Link to="/d/record">水錶登錄</Link></li>
+              <li><Link to="/d/record/history">登錄記錄</Link></li>
+              {user?.title !== Role.ENG && <>
+                <li><Link to="/d/record/out">區外要求</Link></li>
+                <li><Link to="/d/user">人事查詢</Link></li>
+              </>}
+            </ul>
+          </div>
         </div>
         <Outlet />
       </div>
