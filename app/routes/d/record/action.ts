22dc61b00b8f6b9d7bfe41ae8fe2244ff9ec8291
sexyoung <Habuche@gmail.com>
2022-10-11 16:55:28 +0800

    fix: 抄表後等待時間太久

--- app/routes/d/record/action.ts
+++ app/routes/d/record/action.ts
@@ -99,6 +99,7 @@ const verb = {
     projectIdListArr.sort((a, b) => a - b);
 
     const keys = await redis.keys(`${REDIS_PREFIX}:*${projectIdListArr.join(',')}*:search:`);
+    await redis.disconnect();
     
     // const keys = [...new Set([
     //   `${REDIS_PREFIX}:*${projectIdList}*:search:${search}`,
@@ -134,16 +135,16 @@ const verb = {
         },
         where: { area: meter!.area! },
       });
-      await areaCache(meter!.area!, areaListItems[0]._count.area);
+      areaCache(meter!.area!, areaListItems[0]._count.area);
     });
 
-    const summary = await redis.hGetAll(`${REDIS_PREFIX}:${projectIdList}:search:${search}`);
-    await redis.disconnect();
+    // const summary = await redis.hGetAll(`${REDIS_PREFIX}:${projectIdList}:search:${search}`);
+    // await redis.disconnect();
     return {
-      meterCount: +summary.meterCount,
-      meterCountSummary: +summary.meterCountSummary,
-      successCount: +summary.successCount,
-      notRecordCount: +summary.notRecordCount,
+      // meterCount: +summary.meterCount,
+      // meterCountSummary: +summary.meterCountSummary,
+      // successCount: +summary.successCount,
+      // notRecordCount: +summary.notRecordCount,
     };
   }
 }
